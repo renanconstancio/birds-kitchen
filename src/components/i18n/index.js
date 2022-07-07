cb1145782fe02cf79f29d44a67bb20a866f90b91
Renan Constancio <renan@dcisuporte.com.br>
2022-07-07 10:21:44 -0300

    first ajusts

--- src/components/i18n/index.js
+++ src/components/i18n/index.js
@@ -1,36 +1,40 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import translationEN from '../../locales/en/translation.json';
-import translationFR from '../../locales/fr/translation.json';
-import translationDE from '../../locales/de/translation.json';
-import translationIT from '../../locales/it/translation.json';
+import translationBR from "../../locales/br/translation.json";
+import translationEN from "../../locales/en/translation.json";
+import translationFR from "../../locales/fr/translation.json";
+import translationDE from "../../locales/de/translation.json";
+import translationIT from "../../locales/it/translation.json";
 
 // The translations
 const resources = {
-	en: {
-		translation: translationEN
-	},
-	fr: {
-		translation: translationFR
-	},
-	de: {
-		translation: translationDE
-	},
-	it: {
-		translation: translationIT
-	}
+  br: {
+    translation: translationBR,
+  },
+  en: {
+    translation: translationEN,
+  },
+  fr: {
+    translation: translationFR,
+  },
+  de: {
+    translation: translationDE,
+  },
+  it: {
+    translation: translationIT,
+  },
 };
 
-i18n.use( initReactI18next ).init( {
-	resources,
-	lng: 'en',
-	keySeparator: false,
-	fallbackLng: 'en',
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "br",
+  keySeparator: false,
+  fallbackLng: "br",
 
-	interpolation: {
-		escapeValue: false
-	}
-} );
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './components/i18n';
-import App from './app';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./components/i18n";
+import App from "./app";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.querySelector( '#root' )
+  document.querySelector("#root")
 );
 
 serviceWorker.unregister();
